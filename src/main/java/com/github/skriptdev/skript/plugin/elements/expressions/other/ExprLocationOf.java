45f65d6e402230723f2c31eb197cacf41fb70e23
ShaneBeee <shanebolenback@me.com>
2026-01-31 08:49:26 -0800

    ExprLocationOf - add Block

--- src/main/java/com/github/skriptdev/skript/plugin/elements/expressions/other/ExprLocationOf.java
+++ src/main/java/com/github/skriptdev/skript/plugin/elements/expressions/other/ExprLocationOf.java
@@ -1,31 +1,38 @@
 package com.github.skriptdev.skript.plugin.elements.expressions.other;
 
+import com.github.skriptdev.skript.api.hytale.Block;
 import com.hypixel.hytale.math.vector.Location;
 import com.hypixel.hytale.server.core.entity.Entity;
 import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
 import com.hypixel.hytale.server.core.universe.world.World;
 import io.github.syst3ms.skriptparser.lang.properties.PropertyExpression;
 import io.github.syst3ms.skriptparser.registration.SkriptRegistration;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class ExprLocationOf extends PropertyExpression<Entity, Location> {
+public class ExprLocationOf extends PropertyExpression<Object, Location> {
 
     public static void register(SkriptRegistration registration) {
         registration.newPropertyExpression(ExprLocationOf.class, Location.class,
-                "location", "entities")
-            .name("Location of Entity")
-            .description("Returns the location of an entity.")
+                "location", "entities/blocks")
+            .name("Location of Block/Entity")
+            .description("Returns the location of a block or entity.")
             .examples("set {_loc} to location of context-player")
             .since("1.0.0")
             .register();
     }
 
     @Override
-    public @Nullable Location getProperty(Entity entity) {
-        World world = entity.getWorld();
-        assert world != null;
-        TransformComponent transform = entity.getTransformComponent();
-        return new Location(world.getName(), transform.getTransform());
+    public @Nullable Location getProperty(@NotNull Object o) {
+        if (o instanceof Entity entity) {
+            World world = entity.getWorld();
+            assert world != null;
+            TransformComponent transform = entity.getTransformComponent();
+            return new Location(world.getName(), transform.getTransform());
+        } else if (o instanceof Block block) {
+            return block.getLocation();
+        }
+        return null;
     }
 
 }
