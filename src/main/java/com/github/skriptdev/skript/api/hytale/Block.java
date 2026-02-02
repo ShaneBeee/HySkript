6a645a69b4a5cce8c2a454e96e5a116ba7d0a45b
ShaneBeee <shanebolenback@me.com>
2026-02-02 08:33:27 -0800

    Block - remove holder of Type as this may cause issues when a block changes and you're storing this in a variable

--- src/main/java/com/github/skriptdev/skript/api/hytale/Block.java
+++ src/main/java/com/github/skriptdev/skript/api/hytale/Block.java
@@ -14,11 +14,9 @@ import com.hypixel.hytale.server.core.universe.world.chunk.section.FluidSection;
 import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
-
 /**
  * Represents a block in a world.
- * Hytale doesn't appear to have a representation of a block in a world.
+ * Hytale doesn't appear to have a representation of a block in the world.
  * This class provides a wrapper around Hytale's block system, allowing for easy interaction with blocks in a world.
  * This may be changed/removed in the future.
  */
@@ -26,19 +24,16 @@ import java.util.Objects;
 public class Block {
 
     private final @NotNull World world;
-    private @NotNull BlockType type;
     private final @NotNull Vector3i pos;
 
     public Block(@NotNull World world, @NotNull Vector3i pos) {
         this.world = world;
         this.pos = pos;
-        this.type = Objects.requireNonNull(world.getBlockType(pos));
     }
 
     public Block(@NotNull World world, @NotNull Vector3i pos, @NotNull BlockType type) {
         this.world = world;
         this.pos = pos;
-        this.type = type;
     }
 
     public Block(@NotNull Location location) {
@@ -52,11 +47,11 @@ public class Block {
     }
 
     public @NotNull BlockType getType() {
-        return this.type;
+        BlockType blockType = this.world.getBlockType(this.pos);
+        return blockType != null ? blockType : BlockType.EMPTY;
     }
 
     public void setType(@NotNull BlockType type, int settings) {
-        this.type = type;
         this.world.setBlock(this.pos.getX(), this.pos.getY(), this.pos.getZ(), type.getId(), settings);
     }
 
@@ -152,15 +147,15 @@ public class Block {
 
     public String toTypeString() {
         return String.format("[%s] block at (%s,%s,%s) in '%s'",
-            this.type.getId(), this.pos.getX(), this.pos.getY(), this.pos.getZ(), this.world.getName());
+            this.getType().getId(), this.pos.getX(), this.pos.getY(), this.pos.getZ(), this.world.getName());
     }
 
     @Override
     public String toString() {
         return "Block{" +
-            "world=" + world.getName() +
-            ", type=" + type +
-            ", pos=" + pos +
+            "world=" + this.world.getName() +
+            ", type=" + this.getType() +
+            ", pos=" + this.pos +
             '}';
     }
 
