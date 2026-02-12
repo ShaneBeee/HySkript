032f9003621f33b60c6029c195deead9058293c3
ShaneBeee <shanebolenback@me.com>
2026-02-12 15:35:20 -0800

    EffServerShutdown - add shutdown effect

--- src/main/java/com/github/skriptdev/skript/plugin/elements/effects/EffectHandler.java
+++ src/main/java/com/github/skriptdev/skript/plugin/elements/effects/EffectHandler.java
@@ -18,6 +18,7 @@ import com.github.skriptdev.skript.plugin.elements.effects.other.EffCancelEvent;
 import com.github.skriptdev.skript.plugin.elements.effects.other.EffDelay;
 import com.github.skriptdev.skript.plugin.elements.effects.player.EffKick;
 import com.github.skriptdev.skript.plugin.elements.effects.other.EffSendTitle;
+import com.github.skriptdev.skript.plugin.elements.effects.server.EffServerShutdown;
 import com.github.skriptdev.skript.plugin.elements.effects.world.EffChunkRegenerate;
 import com.github.skriptdev.skript.plugin.elements.effects.world.EffExplosion;
 import com.github.skriptdev.skript.plugin.elements.effects.entity.EffInteraction;
@@ -50,6 +51,9 @@ public class EffectHandler {
         EffBan.register(registration);
         EffConnect.register(registration);
         EffKick.register(registration);
+
+        // SERVERE
+        EffServerShutdown.register(registration);
 
         // WORLD
         EffChunkRegenerate.register(registration);
