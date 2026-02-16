ce7ad94b0248c4879c5989ed3175010df66ffec9
ShaneBeee <shanebolenback@me.com>
2026-02-16 14:34:06 -0800

    DefaultFunctions - update to new function registration

--- src/main/java/com/github/skriptdev/skript/plugin/elements/functions/DefaultFunctions.java
+++ src/main/java/com/github/skriptdev/skript/plugin/elements/functions/DefaultFunctions.java
@@ -9,9 +9,6 @@ import com.hypixel.hytale.server.core.asset.type.item.config.Item;
 import com.hypixel.hytale.server.core.inventory.ItemStack;
 import com.hypixel.hytale.server.core.universe.Universe;
 import com.hypixel.hytale.server.core.universe.world.World;
-import io.github.syst3ms.skriptparser.structures.functions.FunctionParameter;
-import io.github.syst3ms.skriptparser.structures.functions.Functions;
-import io.github.syst3ms.skriptparser.structures.functions.JavaFunction;
 import io.github.syst3ms.skriptparser.util.SkriptDate;
 import io.github.syst3ms.skriptparser.util.Time;
 import org.bson.BsonDocument;
@@ -24,30 +21,23 @@ public class DefaultFunctions {
     public static void register(SkriptRegistration reg) {
         dateTimeFunctions(reg);
         itemFunctions(reg);
+        mathFunctions(reg);
         positionFunctions(reg);
     }
 
     private static void dateTimeFunctions(SkriptRegistration reg) {
-        Functions.newJavaFunction(reg, new JavaFunction<>(
-                "date",
-                new FunctionParameter[]{
-                    new FunctionParameter<>("year", Number.class, true),
-                    new FunctionParameter<>("month", Number.class, true),
-                    new FunctionParameter<>("day", Number.class, true)
-                },
-                SkriptDate.class,
-                true
-            ) {
-                @Override
-                public SkriptDate[] executeSimple(Object[][] params) {
-                    Number year = (Number) params[0][0];
-                    Number month = (Number) params[1][0];
-                    Number day = (Number) params[2][0];
-                    LocalDateTime localDateTime = LocalDateTime.of(year.intValue(), month.intValue(),
-                        day.intValue(), 0, 0);
-                    long epochSecond = localDateTime.toEpochSecond(ZoneOffset.systemDefault().getRules().getOffset(localDateTime));
-                    return new SkriptDate[]{SkriptDate.of(epochSecond * 1000)};
-                }
+        reg.newJavaFunction("date", SkriptDate.class, true)
+            .parameter("year", Number.class, true)
+            .parameter("month", Number.class, true)
+            .parameter("day", Number.class, true)
+            .executeSingle(params -> {
+                Number year = (Number) params[0][0];
+                Number month = (Number) params[1][0];
+                Number day = (Number) params[2][0];
+                LocalDateTime localDateTime = LocalDateTime.of(year.intValue(), month.intValue(),
+                    day.intValue(), 0, 0);
+                long epochSecond = localDateTime.toEpochSecond(ZoneOffset.systemDefault().getRules().getOffset(localDateTime));
+                return SkriptDate.of(epochSecond * 1000);
             })
             .name("Date")
             .description("Creates a new Date with the given parameters.")
@@ -55,31 +45,24 @@ public class DefaultFunctions {
             .since("INSERT VERSION")
             .register();
 
-        Functions.newJavaFunction(reg, new JavaFunction<>(
-                "dateTime",
-                new FunctionParameter[]{
-                    new FunctionParameter<>("year", Number.class, true),
-                    new FunctionParameter<>("month", Number.class, true),
-                    new FunctionParameter<>("day", Number.class, true),
-                    new FunctionParameter<>("hour", Number.class, true),
-                    new FunctionParameter<>("minute", Number.class, true),
-                    new FunctionParameter<>("second", Number.class, true)
-                },
-                SkriptDate.class,
-                true) {
-                @Override
-                public SkriptDate[] executeSimple(Object[][] params) {
-                    Number year = (Number) params[0][0];
-                    Number month = (Number) params[1][0];
-                    Number day = (Number) params[2][0];
-                    Number hour = (Number) params[3][0];
-                    Number minute = (Number) params[4][0];
-                    Number second = (Number) params[5][0];
-                    LocalDateTime localDateTime = LocalDateTime.of(year.intValue(), month.intValue(),
-                        day.intValue(), hour.intValue(), minute.intValue(), second.intValue());
-                    long epochSecond = localDateTime.toEpochSecond(ZoneOffset.systemDefault().getRules().getOffset(localDateTime));
-                    return new SkriptDate[]{SkriptDate.of(epochSecond * 1000)};
-                }
+        reg.newJavaFunction("dateTime", SkriptDate.class, true)
+            .parameter("year", Number.class, true)
+            .parameter("month", Number.class, true)
+            .parameter("day", Number.class, true)
+            .parameter("hour", Number.class, true)
+            .parameter("minute", Number.class, true)
+            .parameter("second", Number.class, true)
+            .executeSingle(params -> {
+                Number year = (Number) params[0][0];
+                Number month = (Number) params[1][0];
+                Number day = (Number) params[2][0];
+                Number hour = (Number) params[3][0];
+                Number minute = (Number) params[4][0];
+                Number second = (Number) params[5][0];
+                LocalDateTime localDateTime = LocalDateTime.of(year.intValue(), month.intValue(),
+                    day.intValue(), hour.intValue(), minute.intValue(), second.intValue());
+                long epochSecond = localDateTime.toEpochSecond(ZoneOffset.systemDefault().getRules().getOffset(localDateTime));
+                return SkriptDate.of(epochSecond * 1000);
             })
             .name("DateTime")
             .description("Creates a new Date with a time with the given parameters.",
@@ -88,22 +71,16 @@ public class DefaultFunctions {
             .since("INSERT VERSION")
             .register();
 
-        Functions.newJavaFunction(reg, new JavaFunction<>(
-                "time",
-                new FunctionParameter[]{
-                    new FunctionParameter<>("hour", Number.class, true),
-                    new FunctionParameter<>("minute", Number.class, true),
-                    new FunctionParameter<>("second", Number.class, true)
-                },
-                Time.class,
-                true) {
-                @Override
-                public Time[] executeSimple(Object[][] params) {
-                    Number hour = (Number) params[0][0];
-                    Number minute = (Number) params[1][0];
-                    Number second = (Number) params[2][0];
-                    return new Time[]{Time.of(hour.intValue(), minute.intValue(), second.intValue(), 0)};
-                }
+        reg.newJavaFunction("time", Time.class, true)
+            .parameter("hour", Number.class, true)
+            .parameter("minute", Number.class, true)
+            .parameter("second", Number.class, true)
+            .executeSingle(params -> {
+                Number hour = (Number) params[0][0];
+                Number minute = (Number) params[1][0];
+                Number second = (Number) params[2][0];
+                return Time.of(hour.intValue(), minute.intValue(), second.intValue(), 0);
+
             })
             .name("Time")
             .description("Creates a new Time with the given parameters.",
@@ -114,27 +91,19 @@ public class DefaultFunctions {
     }
 
     private static void itemFunctions(SkriptRegistration reg) {
-        Functions.newJavaFunction(reg, new JavaFunction<>(
-                "itemstack",
-                new FunctionParameter[]{
-                    new FunctionParameter<>("type", Item.class, true),
-                    new FunctionParameter<>("quantity", Number.class, true),
-                    new FunctionParameter<>("durability", Number.class, true),
-                    new FunctionParameter<>("maxDurability", Number.class, true)
-                },
-                ItemStack.class,
-                true) {
-                @Override
-                public ItemStack[] executeSimple(Object[][] params) {
-                    Item type = (Item) params[0][0];
-                    Number quantity = (Number) params[1][0];
-                    Number durability = (Number) params[2][0];
-                    Number maxDurability = (Number) params[3][0];
-                    int max = maxDurability.intValue();
-                    ItemStack itemStack = new ItemStack(type.getId(), quantity.intValue(),
-                        Math.clamp(durability.intValue(), 0, max), max, new BsonDocument());
-                    return new ItemStack[]{itemStack};
-                }
+        reg.newJavaFunction("itemstack", ItemStack.class, true)
+            .parameter("type", Item.class, true)
+            .parameter("quantity", Number.class, true)
+            .parameter("durability", Number.class, true)
+            .parameter("maxDurability", Number.class, true)
+            .executeSingle(params -> {
+                Item type = (Item) params[0][0];
+                Number quantity = (Number) params[1][0];
+                Number durability = (Number) params[2][0];
+                Number maxDurability = (Number) params[3][0];
+                int max = maxDurability.intValue();
+                return new ItemStack(type.getId(), quantity.intValue(),
+                    Math.clamp(durability.intValue(), 0, max), max, new BsonDocument());
             })
             .name("ItemStack")
             .description("Creates a new ItemStack with the given parameters.")
@@ -143,25 +112,57 @@ public class DefaultFunctions {
             .register();
     }
 
+    private static void mathFunctions(SkriptRegistration reg) {
+        reg.newJavaFunction("abs", Number.class, true)
+            .parameter("number", Number.class)
+            .executeSingle(params -> Math.abs(((Number) params[0][0]).doubleValue()))
+            .name("Math - Absolute")
+            .description("Returns the absolute (always positive) value of a number.")
+            .since("INSERT VERSION")
+            .register();
+        reg.newJavaFunction("ceil", Number.class, true)
+            .parameter("number", Number.class)
+            .executeSingle(params -> Math.ceil(((Number) params[0][0]).doubleValue()))
+            .name("Math - Ceiling")
+            .description("Returns the ceiling (rounded up) value of a decimal point number.")
+            .since("INSERT VERSION")
+            .register();
+        reg.newJavaFunction("cos", Number.class, true)
+            .parameter("number", Number.class)
+            .executeSingle(params -> Math.cos(((Number) params[0][0]).doubleValue()))
+            .name("Math - Cosine")
+            .description("Retuns the cosine of an angle.")
+            .since("INSERT VERSION")
+            .register();
+        reg.newJavaFunction("floor", Number.class, true)
+            .parameter("number", Number.class)
+            .executeSingle(params -> Math.floor(((Number) params[0][0]).doubleValue()))
+            .name("Math - Floor")
+            .description("Returns the floor (rounded down) value of a decimal point number.")
+            .since("INSERT VERSION")
+            .register();
+        reg.newJavaFunction("sin", Number.class, true)
+            .parameter("number", Number.class)
+            .executeSingle(params -> Math.sin(((Number) params[0][0]).doubleValue()))
+            .name("Math - Sine")
+            .description("Retuns the sine of an angle.")
+            .since("INSERT VERSION")
+            .register();
+    }
+
     private static void positionFunctions(SkriptRegistration reg) {
-        Functions.newJavaFunction(reg, new JavaFunction<>(
-                "location",
-                new FunctionParameter[]{
-                    new FunctionParameter<>("x", Number.class, true),
-                    new FunctionParameter<>("y", Number.class, true),
-                    new FunctionParameter<>("z", Number.class, true),
-                    new FunctionParameter<>("world", World.class, true)
-                },
-                Location.class,
-                true) {
-                @Override
-                public Location[] executeSimple(Object[][] params) {
-                    Number x = (Number) params[0][0];
-                    Number y = (Number) params[1][0];
-                    Number z = (Number) params[2][0];
-                    World world = (World) params[3][0];
-                    return new Location[]{new Location(world.getName(), x.doubleValue(), y.doubleValue(), z.doubleValue())};
-                }
+        reg.newJavaFunction("location", Location.class, true)
+            .parameter("x", Number.class)
+            .parameter("y", Number.class)
+            .parameter("z", Number.class)
+            .parameter("world", World.class)
+            .executeSingle(params -> {
+                Number x = (Number) params[0][0];
+                Number y = (Number) params[1][0];
+                Number z = (Number) params[2][0];
+                World world = (World) params[3][0];
+                return new Location(world.getName(), x.doubleValue(), y.doubleValue(), z.doubleValue());
+
             })
             .name("Location")
             .description("Creates a location in a world.")
@@ -169,22 +170,15 @@ public class DefaultFunctions {
             .since("1.0.0")
             .register();
 
-        Functions.newJavaFunction(reg, new JavaFunction<>(
-                "vector3i",
-                new FunctionParameter[]{
-                    new FunctionParameter<>("x", Number.class, true),
-                    new FunctionParameter<>("y", Number.class, true),
-                    new FunctionParameter<>("z", Number.class, true)
-                },
-                Vector3i.class,
-                true) {
-                @Override
-                public Vector3i[] executeSimple(Object[][] params) {
-                    Number x = (Number) params[0][0];
-                    Number y = (Number) params[1][0];
-                    Number z = (Number) params[2][0];
-                    return new Vector3i[]{new Vector3i(x.intValue(), y.intValue(), z.intValue())};
-                }
+        reg.newJavaFunction("vector3i", Vector3i.class, true)
+            .parameter("x", Number.class)
+            .parameter("y", Number.class)
+            .parameter("z", Number.class)
+            .executeSingle(params -> {
+                Number x = (Number) params[0][0];
+                Number y = (Number) params[1][0];
+                Number z = (Number) params[2][0];
+                return new Vector3i(x.intValue(), y.intValue(), z.intValue());
             })
             .name("Vector3i")
             .description("Creates a vector3i with integer coordinates.")
@@ -192,22 +186,15 @@ public class DefaultFunctions {
             .since("1.0.0")
             .register();
 
-        Functions.newJavaFunction(reg, new JavaFunction<>(
-                "vector3f",
-                new FunctionParameter[]{
-                    new FunctionParameter<>("x", Number.class, true),
-                    new FunctionParameter<>("y", Number.class, true),
-                    new FunctionParameter<>("z", Number.class, true)
-                },
-                Vector3f.class,
-                true) {
-                @Override
-                public Vector3f[] executeSimple(Object[][] params) {
-                    Number x = (Number) params[0][0];
-                    Number y = (Number) params[1][0];
-                    Number z = (Number) params[2][0];
-                    return new Vector3f[]{new Vector3f(x.floatValue(), y.floatValue(), z.floatValue())};
-                }
+        reg.newJavaFunction("vector3f", Vector3f.class, true)
+            .parameter("x", Number.class)
+            .parameter("y", Number.class)
+            .parameter("z", Number.class)
+            .executeSingle(params -> {
+                Number x = (Number) params[0][0];
+                Number y = (Number) params[1][0];
+                Number z = (Number) params[2][0];
+                return new Vector3f(x.floatValue(), y.floatValue(), z.floatValue());
             })
             .name("Vector3f")
             .description("Creates a vector3f with float coordinates.")
@@ -215,22 +202,15 @@ public class DefaultFunctions {
             .since("1.0.0")
             .register();
 
-        Functions.newJavaFunction(reg, new JavaFunction<>(
-                "vector3d",
-                new FunctionParameter[]{
-                    new FunctionParameter<>("x", Number.class, true),
-                    new FunctionParameter<>("y", Number.class, true),
-                    new FunctionParameter<>("z", Number.class, true)
-                },
-                Vector3d.class,
-                true) {
-                @Override
-                public Vector3d[] executeSimple(Object[][] params) {
-                    Number x = (Number) params[0][0];
-                    Number y = (Number) params[1][0];
-                    Number z = (Number) params[2][0];
-                    return new Vector3d[]{new Vector3d(x.doubleValue(), y.doubleValue(), z.doubleValue())};
-                }
+        reg.newJavaFunction("vector3d", Vector3d.class, true)
+            .parameter("x", Number.class)
+            .parameter("y", Number.class)
+            .parameter("z", Number.class)
+            .executeSingle(params -> {
+                Number x = (Number) params[0][0];
+                Number y = (Number) params[1][0];
+                Number z = (Number) params[2][0];
+                return new Vector3d(x.doubleValue(), y.doubleValue(), z.doubleValue());
             })
             .name("Vector3d")
             .description("Creates a vector3d with double coordinates.")
@@ -238,19 +218,9 @@ public class DefaultFunctions {
             .since("1.0.0")
             .register();
 
-        Functions.newJavaFunction(reg, new JavaFunction<>(
-                "world",
-                new FunctionParameter[]{
-                    new FunctionParameter<>("name", String.class, true)
-                },
-                World.class,
-                true) {
-                @Override
-                public World[] executeSimple(Object[][] params) {
-                    String name = (String) params[0][0];
-                    return new World[]{Universe.get().getWorld(name)};
-                }
-            })
+        reg.newJavaFunction("world", World.class, true)
+            .parameter("name", String.class)
+            .executeSingle(params -> Universe.get().getWorld((String) params[0][0]))
             .name("World")
             .description("Returns the world with the given name.")
             .examples("set {_world} to world(\"default\")")
