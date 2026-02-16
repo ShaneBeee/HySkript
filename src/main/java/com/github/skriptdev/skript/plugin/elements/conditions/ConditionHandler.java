e9d4f97db39380aeb4494905c0df90eac0938166
ShaneBeee <shanebolenback@me.com>
2026-02-16 11:05:17 -0800

    CondEntityIsAlive - add condition to check for life

--- src/main/java/com/github/skriptdev/skript/plugin/elements/conditions/ConditionHandler.java
+++ src/main/java/com/github/skriptdev/skript/plugin/elements/conditions/ConditionHandler.java
@@ -2,10 +2,14 @@ package com.github.skriptdev.skript.plugin.elements.conditions;
 
 
 import com.github.skriptdev.skript.api.skript.registration.SkriptRegistration;
+import com.github.skriptdev.skript.plugin.elements.conditions.entity.CondEntityIsAlive;
 
 public class ConditionHandler {
 
     public static void register(SkriptRegistration registration) {
+        // ENTITY
+        CondEntityIsAlive.register(registration);
+
         CondEntityIsFrozen.register(registration);
         CondHasPermission.register(registration);
         CondInventoryCanHold.register(registration);
