c71f1366e52aa5c158425a1f2c36fdfdbc381889
ShaneBeee <shanebolenback@me.com>
2026-02-19 11:19:34 -0800

    EvtPlayerPickupItem - add

--- src/main/java/com/github/skriptdev/skript/plugin/elements/events/EventHandler.java
+++ src/main/java/com/github/skriptdev/skript/plugin/elements/events/EventHandler.java
@@ -25,6 +25,7 @@ import com.github.skriptdev.skript.plugin.elements.events.player.EvtPlayerDropIt
 import com.github.skriptdev.skript.plugin.elements.events.player.EvtPlayerJoin;
 import com.github.skriptdev.skript.plugin.elements.events.player.EvtPlayerMouseClick;
 import com.github.skriptdev.skript.plugin.elements.events.player.EvtPlayerMouseMove;
+import com.github.skriptdev.skript.plugin.elements.events.player.EvtPlayerPickupItem;
 import com.github.skriptdev.skript.plugin.elements.events.player.EvtPlayerPlaceBlock;
 import com.github.skriptdev.skript.plugin.elements.events.player.EvtPlayerSetupConnect;
 import com.github.skriptdev.skript.plugin.elements.events.player.EvtPlayerSetupDisconnect;
@@ -63,6 +64,7 @@ public class EventHandler {
         EvtPlayerJoin.register(registration);
         EvtPlayerMouseClick.register(registration);
         EvtPlayerMouseMove.register(registration);
+        EvtPlayerPickupItem.register(registration);
         EvtPlayerPlaceBlock.register(registration);
         EvtPlayerSetupConnect.register(registration);
         EvtPlayerSetupDisconnect.register(registration);
