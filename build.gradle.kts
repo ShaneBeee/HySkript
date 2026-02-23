ad30e87abd538034e2786ec6ac353b457dee8906
ShaneBeee <shanebolenback@me.com>
2026-02-23 15:45:24 -0800

    build.gradle.kts - da ferq happened here

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,11 +32,7 @@ dependencies {
     compileOnly("com.hypixel.hytale:Server:${hytaleVersion}")
     compileOnly("org.jetbrains:annotations:26.0.2")
     testImplementation("com.google.code.gson:gson:2.13.2")
-<<<<<<< dev/feature
-    implementation("com.github.SkriptDev:skript-parser:dev~patch-SNAPSHOT") {
-=======
     implementation("com.github.SkriptDev:skript-parser:1.0.10") {
->>>>>>> master
         isTransitive = false
     }
     implementation("com.github.Zoltus:TinyMessage:2.0.1") {
