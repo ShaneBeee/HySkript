bfb3d775add10187e093f6b66b4646bbc769923e
ShaneBeee <shanebolenback@me.com>
2026-02-27 19:02:15 -0800

    EvtBoot - fix not being triggered

--- src/main/java/com/github/skriptdev/skript/plugin/elements/events/server/EvtBoot.java
+++ src/main/java/com/github/skriptdev/skript/plugin/elements/events/server/EvtBoot.java
@@ -1,12 +1,9 @@
 package com.github.skriptdev.skript.plugin.elements.events.server;
 
 import com.github.skriptdev.skript.api.skript.registration.SkriptRegistration;
-import com.github.skriptdev.skript.plugin.HySk;
-import com.hypixel.hytale.event.EventRegistration;
 import com.hypixel.hytale.server.core.event.events.BootEvent;
 import io.github.syst3ms.skriptparser.lang.Expression;
 import io.github.syst3ms.skriptparser.lang.TriggerContext;
-import io.github.syst3ms.skriptparser.lang.TriggerMap;
 import io.github.syst3ms.skriptparser.lang.event.SkriptEvent;
 import io.github.syst3ms.skriptparser.parsing.ParseContext;
 import org.jetbrains.annotations.NotNull;
@@ -15,22 +12,15 @@ public class EvtBoot extends SkriptEvent {
 
     public static void register(SkriptRegistration reg) {
         reg.newEvent(EvtBoot.class, "boot", "server boot")
+            .setHandledContexts(BootContext.class)
             .name("Server Boot")
-            .description("Called when the server is starting up.")
+            .description("Called when the server is done starting up.")
             .since("1.0.0")
             .register();
     }
 
-    private static EventRegistration<Void, BootEvent> LISTENER;
-
     @Override
     public boolean init(Expression<?>[] expressions, int matchedPattern, ParseContext parseContext) {
-        if (LISTENER == null) {
-            LISTENER = HySk.getInstance().getEventRegistry().registerGlobal(BootEvent.class, event -> {
-                BootContext bootContext = new BootContext(event);
-                TriggerMap.callTriggersByContext(bootContext);
-            });
-        }
         return true;
     }
 
@@ -44,7 +34,7 @@ public class EvtBoot extends SkriptEvent {
         return "server boot event";
     }
 
-    private record BootContext(BootEvent event) implements TriggerContext {
+    public record BootContext(BootEvent event) implements TriggerContext {
         @Override
         public String getName() {
             return "boot context";
